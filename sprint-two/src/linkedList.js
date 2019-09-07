3a0dcb72fb43a292cdb4299c5b6dc49810a6ba5a
majimmy88 <majimmy88@gmail.com>
2019-09-06 22:15:49 -0700

    Finished Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,38 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (list.tail === null) {
+      list.head = Node(value);
+      list.tail = list.head;
+    } else {
+      let newNode = Node(value);
+      list.tail.next = newNode;
+      list.tail = newNode;
+
+    }
+
   };
 
   list.removeHead = function() {
+    if (list.head !== null) {
+      var value = list.head.value;
+      list.head = list.head.next;
+      return value;
+    }
   };
 
   list.contains = function(target) {
+    var linked = false;
+    var check = function(node) {
+      if(node.value === target) {
+        linked = true;
+      } if(node.next === null) {
+          return linked;
+      } else {
+        return check(node.next);
+      }
+    }
+    return check(list.head);
   };
 
   return list;
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,10 @@
 var Tree = function(value) {
   var newTree = {};
+  _.extend(newTree, treeMethods);
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
 
   return newTree;
 };
@@ -11,9 +12,25 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = {};
+  child.value = value;
+  child.children = [];
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  var inside = false;
+  var check = function(node) {
+    if(node.value === target){
+      inside = true;
+    } else if(node.children.length > 0){
+      node.children.forEach(function(el){
+        check(el);
+      })
+    }
+    return inside;
+  };
+  check(this);
 };
 
 
