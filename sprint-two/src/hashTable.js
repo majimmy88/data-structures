5d2fafdbd81fd07ab895b5b424b500c55f7dbb15
majimmy88 <majimmy88@gmail.com>
2019-09-07 18:25:29 -0700

    Working on Hash tables

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -8,22 +8,23 @@ var HashTable = function() {
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 
-  for (var i = 0; i < this._storage.length; i++) {
-    if (i === index) {
-      this._storage[i].push ([k, v]);
-    }
-  }
+  // for (var i = 0; i < this._storage.length; i++) {
+  //   if (i === index) {
+  //     this._storage[i].push ([k, v]);
+  //   }
+  // }
+  console.log(this._storage[index])
+  console.log(this)
+  this._storage[index] = []
+  this._storage[index].push ([k, v]);
+
 
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var arrLength = this._storage.length;
-  for (let i = 0; i < arrLength; i++) {
-    if(arrLength[i] === k) {
-      return arrLength[i][1];
-    }
-  }
+  return this._storage[index][0][1]
 };
 
 HashTable.prototype.remove = function(k) {
